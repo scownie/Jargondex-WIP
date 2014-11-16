2540be3f56ec6a987bb7cbacbc9dbdb6286ac09f
scownie <stevecownie@gmail.com>
2014-11-16 16:41:46 +0000

    Added more brands

--- src/Scraper.java
+++ src/Scraper.java
@@ -17,12 +17,6 @@ public class Scraper {
 
 	private static ArrayList<Company> Companies = new ArrayList<Company>();
 
-	public static ArrayList<Company> getCompanies() {
-		return Companies;
-	}
-
-
-
 	public static void main(String[] args) throws IOException {
 
 		Company Samsung = new Company("Samsung", "class", "news_wrap", "http://www.samsung.com/uk/news/local/");
@@ -39,19 +33,41 @@ public class Scraper {
 		Companies.add(Amazon);
 		Company Sony = new Company("Sony", "class", "textParagraph", "http://www.sony.net/SonyInfo/News/Press/");
 		Companies.add(Sony);
-		Company Panasonic = new Company("Panasonic", "class", "newsroom-article", "http://news.panasonic.co.uk/pressreleases");
+		Company Panasonic = new Company("Panasonic", "class", "newsroom-article", "http://news.panasonic.co.uk/pressreleases/");
 		Companies.add(Panasonic);
 		Company Google = new Company("Google", "class", "post-content", "http://www.googlepress.blogspot.co.uk/");
 		Companies.add(Google);
-		Company Dell = new Company("Dell", "class", "uif_maincontent", "http://www.dell.com/learn/uk/en/ukcorp1/newsroom-press-releases");
+		Company Dell = new Company("Dell", "class", "uif_maincontent", "http://www.dell.com/learn/uk/en/ukcorp1/newsroom-press-releases/");
 		Companies.add(Dell);
 		Company Toshiba = new Company("Toshiba", "class", "pressReleaseText", "http://www.toshiba.co.uk/press/releases/");
 		Companies.add(Toshiba);
-		Company LG = new Company("LG", "class", "column2", "http://www.lg.com/uk/press-release");
+		Company LG = new Company("LG", "class", "column2", "http://www.lg.com/uk/press-release/");
 		Companies.add(LG);
 		Company Intel = new Company("Intel", "class", "jive-content-body", "http://newsroom.intel.com/community/en_uk/blog/");
 		Companies.add(Intel);	
-
+		Company Oracle = new Company("Oracle", "class", "news-details-body", "https://emeapressoffice.oracle.com/Press-Releases/");
+		Companies.add(Oracle);
+		Company SAP = new Company("SAP", "class", "article", "http://www.news-sap.com/topics/press-release/");
+		Companies.add(SAP);
+		Company Symantec = new Company("Symantec", "class", "bckPadMedium", "http://www.symantec.com/en/uk/about/news/release/");
+		Companies.add(Symantec);
+		Company VMWare = new Company("VMWare", "class", "mw_release", "http://www.vmware.com/uk/company/news/releases/");
+		Companies.add(VMWare);
+		Company CATechnologies = new Company("CATechnologies", "class", "container", "http://www.ca.com/sg/news/press-releases/");
+		Companies.add(CATechnologies);
+		Company Intuit = new Company("Intuit", "id", "pressRelease", "http://about.intuit.com/about_intuit/press_room/");
+		Companies.add(Intuit);
+		Company Salesforce = new Company("Salesforce", "class", "category", "http://www.salesforce.com/uk/company/news-press/press-releases/");
+		Companies.add(Salesforce);
+		Company Cisco = new Company("Cisco", "id", "releasecopy", "http://newsroom.cisco.com/press-release-content?");
+		Companies.add(Cisco);
+		Company Siemens = new Company("Siemens", "class", "left-content", "http://www.siemens.co.uk/en/news_press/index/news_archive/");
+		Companies.add(Siemens);
+		Company EMC = new Company("EMC", "class", "news-wrapper", "http://uk.emc.com/about/news/press/");
+		Companies.add(EMC);
+		
+		
+		
 		// This gets text based on CSS selector defined for one particular Company in Companies ArrayList
 
 		String contenttoPrint = null;
@@ -64,31 +80,32 @@ public class Scraper {
 		for (Element e : content) {
 			contenttoPrint = e.text();
 		}
-	
 
-	try {
+		//BufferedReader & FileReader to print stuff out
 
-		String content2 = contenttoPrint;
+		try {
 
-		File file = new File("/Users/Steve/Workspace/ScraperTest/Filewriter/Filewriter.txt");
+			String content2 = contenttoPrint;
 
-		// if file doesn't exists, then create it
-		if (!file.exists()) {
-			file.createNewFile();
+			File file = new File("/Users/Steve/Workspace/ScraperTest/Filewriter/Filewriter.txt");
+
+			// if file doesn't exist, create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content2);
+			bw.close();
+
+			System.out.println("Done");
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
-		FileWriter fw = new FileWriter(file.getAbsoluteFile());
-		BufferedWriter bw = new BufferedWriter(fw);
-		bw.write(content2);
-		bw.close();
-
-		System.out.println("Done");
-
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-
-	/**
+		/**
 		Document doc = Jsoup.connect(Companies.get(Companies.indexOf(Intel)).getBaseURL()).get();
 		Elements content = doc.getElementsByClass("jive-content-body");
 
@@ -97,7 +114,7 @@ public class Scraper {
 		}
 
 
-		/**
+
 		//This gets all text with a "p" tag
 		for (Company comp: Companies) {
 			String URL = comp.getBaseURL();
@@ -121,8 +138,8 @@ public class Scraper {
 					System.out.println(comp.getCompanyName() + " " + linkHref);
 				}
 		}
-	 */
-}
+		 */
+	}
 }
 
 
